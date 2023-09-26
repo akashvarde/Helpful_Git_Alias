d3aa3fccb7bf4f470c175ca98f87e47240115f25
akashvarde <86535870+akashvarde@users.noreply.github.com>
2023-09-26 15:56:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,18 @@
 # Define ANSI color escape codes
 GREEN='\033[0;32m'   # Green color
 RED='\033[0;31m'     # Red color 
-YELLOW='\033[0;33m'  # Yello color
+YELLOW='\033[0;33m'  # Yellow color
 NC='\033[0m'          # No color (reset)
-log_error(){
+
+log_error() {
     echo "${RED}$1${NC}"
 }
-log_warning(){
+
+log_warning() {
     echo "${YELLOW}$1${NC}"
 }
-log_success(){
+
+log_success() {
     echo "${GREEN}$1${NC}"
 }
 
@@ -24,61 +27,45 @@ folder_name="helpful_git_alias"
 # Define the directory to install custom Git commands globally
 GLOBAL_BIN_DIR="/usr/local/bin/$folder_name"
 
-
-
+# Remove existing 'helpful_git_alias' directory
 rm -rf helpful_git_alias
 
 # Check if the script is running with superuser privileges
 if [ "$(id -u)" -ne 0 ]; then
-  echo "${RED}Error: This script requires superuser/sudo privileges to run. Re-run installation with sudo. ${NC}"
-  exit 1
-  else 
-  echo "${YELLOW}Hi, Thanks for installing :), Hope it helps your work ${NC}"
+    echo "${RED}Error: This script requires superuser/sudo privileges to run. Re-run installation with sudo.${NC}"
+    exit 1
+else 
+    echo "${YELLOW}Hi, Thanks for installing :), Hope it helps your work${NC}"
 fi
 
-
-# Akash Varde :  Create folder for storing git commands 
 # Check if the folder already exists
 if [ -f "/usr/local/bin/git-createdeployement" ]; then
-    
-    # COMMENTING THIS CHANGES AS THIS WILL NOT WORK ON A SH SHELL
-      # echo "Looks like you have earlier installed our program. Do you want to proceed with the update/reinstall? (y/n)"
-      # read choice  
-    # Check the user's choice
-      # if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
-      # echo "${GREEN}Updating...${NC}"
-    # Add your update logic here
-      # elif [ "$choice" = "n" ] || [ "$choice" = "N" ]; then
-      # echo "${RED}Update cancelled.${NC}"
-      # exit 1
-      # else
-      # echo "${RED}Invalid choice. \nAborting...${NC}"
-      # exit 1
-      # fi
-  log_warning "Looks like you have earlier installed the git scripts. Updating..."
-
+    log_warning "Looks like you have earlier installed the git scripts. Updating..."
 fi
 
 echo "${GREEN}Installing...${NC}"
 git clone https://github.com/akashvarde/helpful_git_alias.git --depth=1 || {
-  echo >&2 "${RED}Clone failed with $?${NC}"
-  exit 1
+    echo >&2 "${RED}Clone failed with $?${NC}"
+    exit 1
 }
+
 # Create the folder
 cd helpful_git_alias || {
-  echo >&2 "Unable to create installer $?"
-  exit 1
+    echo >&2 "Unable to create installer $?"
+    exit 1
 }
-#Install
+
+# Install
 make install || {
-echo >&2 "${RED}Installation failed ! $?${NC}"
-exit 1
+    echo >&2 "${RED}Installation failed ! $?${NC}"
+    exit 1
 }
-#delete installer
+
+# Delete installer
 cd ..
 rm -rf helpful_git_alias
 
-echo "Added alises to PATH Variable ...."
+echo "Added aliases to PATH Variable ...."
 echo "Custom Git commands installed successfully!"
-log_success "You can now use command `git createdeployement <target_branch>`"
-exit 1
+log_success "You can now use the command 'git createdeployement <target_branch>'"
+exit 0  # Exiting with 0 indicates a successful script execution
